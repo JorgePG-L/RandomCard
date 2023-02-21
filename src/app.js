46963a668a78f3f0e9c5254a725c53e4bcf6ccbc
Jorge Piqueras García-Ligero <jorgepiqueras92@gmail.com>
2023-02-21 19:45:11 +0000

    feat: First commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,65 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
 };
+const symbols = ["♦", "♥", "♠", "♣"];
+const numbers = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
+const card = document.getElementById("card");
+const height = document.getElementById("alto");
+const width = document.getElementById("ancho");
+const button = document.getElementById("generador");
+const symbolTop = document.getElementById("topSymbol");
+const centerNumber = document.getElementById("number");
+const bottomTop = document.getElementById("bottomSymbol");
+
+function setRandom(arr) {
+  let number = Math.floor(Math.random() * arr.length);
+  return arr[number];
+}
+
+function sameColor() {
+  let equal = setRandom(symbols);
+  if (equal == "♦" || equal == "♥") {
+    symbolTop.style.color = "red";
+    bottomTop.style.color = "red";
+    centerNumber.style.color = "red";
+    button.style.color = "red";
+  } else {
+    symbolTop.style.color = "black";
+    bottomTop.style.color = "black";
+    centerNumber.style.color = "black";
+    button.style.color = "black";
+  }
+  return equal;
+}
+
+function mostrar() {
+  let palo = sameColor();
+  symbolTop.innerText = palo;
+  centerNumber.innerText = setRandom(numbers);
+  bottomTop.innerText = palo;
+  card.style.height = `${height.value}px`;
+  card.style.width = `${width.value}px`;
+}
+
+mostrar();
+
+setInterval(mostrar, 10000);
+
+button.addEventListener("click", mostrar);
